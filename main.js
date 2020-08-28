d1d8cd68600d59c2899735aa6ed5cf9a7046764d
Fabrizio <fabriziospoto1@gmail.com>
2020-08-28 16:12:12 +0200

    Parziale
    Parziale

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo
+// a disposizione una decina di dischi musicali.
+// Servendoci di handlebars stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+
 $(document).ready(function() {
-	//Code
+    // Richiamo ajax
+    $.ajax(
+        {
+            url: "https://flynn.boolean.careers/exercises/api/array/music",
+            method: "GET",
+            success: function (data) {
+                var music = data.response;
+				for (var i = 0; i < music.length; i++) {
+                    // Stampo a schermo con Handlebars
+					var source = document.getElementById("entry-template").innerHTML;
+	                var template = Handlebars.compile(source);
+					var context = music[i];
+					var html = template(context);
+					$('.cds-container').append(html)
+				}
+            },
+            error: function () {
+                alert("E' avvenuto un errore. " + errore);
+            }
+        }
+    );
+    // Selezione genere
+    var genere = $('select').val();
+    if (genere ==  ) {
+        
+    }
 });
